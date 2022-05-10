518c4681ebb1dd6f0fb6d1861f6edb93365c0c1d
Carlos Raez <carlosraez@gmail.com>
2022-05-10 20:41:19 +0200

    Making HomePage With colors Background

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
+import ImageColors from 'react-native-image-colors';
+import { SimplePokemon } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
+
+const windowWidth = Dimensions.get('window').width;
+
+interface Props {
+  pokemon: SimplePokemon;
+}
+
+export const PokemonCard = ({ pokemon }: Props) => {
+  const [bgColor, setBgColor] = useState('grey');
+
+  useEffect(() => {
+    //IOS: Background
+    //Android: Dominant
+    ImageColors.getColors(pokemon.picture, { fallback: 'grey' }).then(
+      colors => {
+        colors.platform === 'android'
+          ? setBgColor(colors.dominant || 'grey')
+          : setBgColor(colors.background || 'grey');
+      },
+    );
+  }, []);
+
+  return (
+    <TouchableOpacity activeOpacity={0.9}>
+      <View
+        style={{
+          ...styles.cardContainer,
+          width: windowWidth * 0.4,
+          backgroundColor: bgColor,
+        }}>
+        <View>
+          <Text style={styles.name}>
+            {pokemon.name} {'\n#' + pokemon.id}
+          </Text>
+        </View>
+        <View style={styles.pokebolaContainer}>
+          <Image
+            source={require('../assets/pokebola-blanca.png')}
+            style={styles.pokebola}
+          />
+        </View>
+        <FadeInImage uri={pokemon.picture} style={styles.pokemonImage} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginHorizontal: 10,
+    height: 120,
+    marginBottom: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
+  },
+  name: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    top: 20,
+    left: 10,
+  },
+  pokebolaContainer: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    opacity: 0.5,
+    overflow: 'hidden',
+  },
+  pokebola: {
+    width: 100,
+    height: 100,
+    right: -10,
+    bottom: -20,
+    position: 'absolute',
+  },
+  pokemonImage: {
+    width: 120,
+    height: 120,
+    position: 'absolute',
+    right: -8,
+    bottom: -5,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,41 +1,52 @@
 import React from 'react';
 
-import { View, Image, FlatList, ActivityIndicator } from 'react-native';
+import { View, Image, FlatList, ActivityIndicator, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { styles } from '../theme/appTheme';
 import { useGetPokemons } from '../hooks/useGetPokemons';
+import { PokemonCard } from '../components/PokemonCard';
 
 export const HomeScreen = () => {
   const { simplePokemonList, loadPokemons } = useGetPokemons();
 
   const { top } = useSafeAreaInsets();
   return (
-    <View>
+    <>
       <Image
         source={require('../assets/pokebola.png')}
         style={styles.pokeBolaBG}
       />
-      <FlatList
-        data={simplePokemonList}
-        keyExtractor={pokemon => pokemon.id}
-        renderItem={({ item }) => (
-          <Image
-            source={{ uri: item.picture }}
-            style={{ width: 100, height: 100 }}
-          />
-        )}
-        onEndReached={loadPokemons}
-        onEndReachedThreshold={0.4}
-        showsVerticalScrollIndicator={false}
-        ListFooterComponent={
-          <ActivityIndicator style={{ height: 100 }} size={20} color="grey" />
-        }
-      />
-    </View>
+      <View
+        style={{
+          ...styles.globalMargin,
+          alignItems: 'center',
+        }}>
+        <FlatList
+          data={simplePokemonList}
+          keyExtractor={pokemon => pokemon.id}
+          numColumns={2}
+          ListHeaderComponent={
+            <Text
+              style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                paddingBottomg: 10,
+                top: top + 20,
+                marginBottom: top + 20,
+              }}>
+              Pokedex
+            </Text>
+          }
+          renderItem={({ item }) => <PokemonCard pokemon={item} />}
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          showsVerticalScrollIndicator={false}
+          ListFooterComponent={
+            <ActivityIndicator style={{ height: 100 }} size={20} color="grey" />
+          }
+        />
+      </View>
+    </>
   );
 };
-
-/* <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20 }}>
-        Pokedex
-      </Text> */
